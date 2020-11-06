4461dd7bdbc5b1e96b3a93c4277f24e0ec47e012
Krzysztof <krzysztofs012@outlook.com>
2020-11-06 22:31:45 +0100

    Created useful functions that helps with compressing photos, saving photos in db and client-side input photo validation

---
+++ src/utils/functions.js
@@ -0,0 +1,29 @@
+import compress from 'browser-image-compression';
+import { firebaseStorage } from '../../config/firebaseConfig';
+import { prefferedImageExtensions } from './constants';
+import { compressionOptions } from './constants';
+
+export const getCurrentYear = () => new Date().getFullYear();
+
+export const isFileImage = (file) => {
+  return file ? prefferedImageExtensions.includes(file['type']) : false;
+};
+
+export const compressPhoto = async (photo) => {
+  try {
+    const compressedPhoto = await compress(photo, compressionOptions);
+    return compressedPhoto;
+  } catch (err) {
+    return false;
+  }
+};
+
+export const savePhotoInDB = async (file, name) => {
+  try {
+    const snapshot = await firebaseStorage.ref(`usersPhotos/${name}`).put(file);
+    console.log(snapshot);
+    return snapshot.state;
+  } catch (err) {
+    return err.message;
+  }
+};
