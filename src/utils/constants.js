1a8fb5f5da5ce11c14a7bf04228be0ad654e857e
Krzysztof <krzysztofs012@outlook.com>
2020-11-06 22:29:39 +0100

    Declared image compressing options and user's photo extensions

---
+++ src/utils/constants.js
@@ -0,0 +1,9 @@
+const compressionOptions = {
+  maxSizeMB: 1,
+  maxWidthOrHeight: 600,
+  useWebWorker: true,
+};
+
+const prefferedImageExtensions = ['image/png', 'image/jpg', 'image/jpeg'];
+
+export { compressionOptions, prefferedImageExtensions };
