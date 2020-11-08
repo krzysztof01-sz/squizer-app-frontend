0f8caf1d32bc4e6d674da6358379d6fc2a7a3c9b
Krzysztof <krzysztofs012@outlook.com>
2020-11-08 23:25:25 +0100

    Added stylesheet link

--- src/pages/global/Header/Header.js
+++ src/pages/global/Header/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Logo from '../../../assets/images/Logo.svg';
+import '../../../styles/global/Components/Header.scss';
 
 const Header = () => {
   return (
     <header className="header">
-      <Link to="/">
+      <Link tabIndex="-1" to="/">
         <img className="header__logo" src={Logo} />
       </Link>
     </header>
