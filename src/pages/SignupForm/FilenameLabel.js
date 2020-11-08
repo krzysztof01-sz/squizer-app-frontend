30d48d3c1914c3225a4f63226552b94268714f81
Krzysztof <krzysztofs012@outlook.com>
2020-11-08 23:20:32 +0100

    Changed stylesheet path

--- src/pages/SignupForm/FilenameLabel.js
+++ src/pages/SignupForm/FilenameLabel.js
@@ -1,3 +1,5 @@
+import '../../styles/pages/SignupForm/FilenameLabel.scss';
+
 const FilenameLabel = ({ userPhoto }) =>
   userPhoto?.name ? <p className="filenameLabel">{userPhoto.name}</p> : <p className="filenameLabel">{userPhoto}</p>;
 
