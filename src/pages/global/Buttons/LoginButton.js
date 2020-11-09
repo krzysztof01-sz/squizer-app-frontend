efe95cad8347b91eb33e64f135a323668345c533
Krzysztof <krzysztofs012@outlook.com>
2020-11-09 23:38:46 +0100

    Accessibility improvements

--- src/pages/global/Buttons/LoginButton.js
+++ src/pages/global/Buttons/LoginButton.js
@@ -1,10 +1,16 @@
+import '../../../styles/global/Buttons/LoginButton.scss';
+
 const LoginButton = ({ isDisabled }) => {
   return isDisabled ? (
     <button className="button loginButton" disabled>
       Login
     </button>
   ) : (
-    <button className="button loginButton">Login</button>
+    <button tabIndex="-1" className="button loginButton">
+      <span role="button" className="loginButton--accessible" aria-controls="filename" tabIndex="0">
+        Login
+      </span>
+    </button>
   );
 };
 
--- src/pages/global/Buttons/SignupButton.js
+++ src/pages/global/Buttons/SignupButton.js
@@ -1,10 +1,16 @@
+import '../../../styles/global/Buttons/SignupButton.scss';
+
 const SingupButton = ({ isDisabled }) => {
   return isDisabled ? (
     <button className="button signupButton" disabled>
       Sign up
     </button>
   ) : (
-    <button className="button signupButton">Sign up</button>
+    <button tabIndex="-1" className="button signupButton">
+      <span role="button" className="signupButton--accessible" aria-controls="filename" tabIndex="0">
+        Sign up
+      </span>
+    </button>
   );
 };
 
