311da226e80e344150e5b90315aee35e3e8894c1
Krzysztof <krzysztofs012@outlook.com>
2020-11-20 21:44:50 +0100

    Create a navigation

---
+++ src/pages/Dashboard/Nav.js
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+
+import { ChevronIcon } from '../../global/Icons/icons';
+import '../../styles/pages/Dashboard/Nav.scss';
+
+const Nav = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <nav className="navigation">
+      <header onClick={() => setIsOpen(!isOpen)} className="navigation__header">
+        menu <ChevronIcon />
+      </header>
+      <ul className="navigation__list" style={{ display: `${isOpen ? 'block' : 'none'}` }}>
+        <li className="navigation__item">add a new quiz</li>
+        <li className="navigation__item">user profile</li>
+        <li className="navigation__item">about</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
