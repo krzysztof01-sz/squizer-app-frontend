1b46767c5db42d0f5b87fd8d6d7233b859bb09f2
Krzysztof <krzysztofs012@outlook.com>
2020-11-20 21:35:36 +0100

    Deleted a reduntation in auth methods - created a factory function whick creates a request method. Added new methods: logout

--- src/api/userAPI.js
+++ src/api/userAPI.js
@@ -1,7 +1,26 @@
+import { firebaseStorage } from '../../config/firebaseConfig';
+
 const baseURL = 'http://localhost:8080';
 
+const getRequestObject = () => {
+  const authToken = localStorage.getItem('auth-token');
+  return {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+      'auth-token': authToken,
+    },
+  };
+};
+
+export const getToken = async () => {
+  const data = await fetch(`${baseURL}/api/getcsrf`);
+  const { csrfToken } = await data.json();
+  return csrfToken;
+};
+
 export const addUser = async (formData, csrfToken) => {
-  console.log(formData);
   const feedback = await fetch(`${baseURL}/api/adduser`, {
     method: 'POST',
     headers: {
@@ -29,28 +48,36 @@ export const loginUser = async (formData, csrfToken) => {
   return await feedback.json();
 };
 
-export const knockTo = async (page, authToken) => {
-  const response = await fetch(`${baseURL}/${page}`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      Accept: 'application/json',
-      'auth-token': authToken,
-    },
-  });
+export const logoutUser = () => {
+  localStorage.removeItem('auth-token');
+  location.reload();
+};
+
+export const knockTo = async (page) => {
+  const responseObject = getRequestObject();
+  const response = await fetch(`${baseURL}/${page}`, responseObject);
 
   return await response.json();
 };
 
-export const getToken = async () => {
-  const data = await fetch(`${baseURL}/api/getcsrf`);
-  const { csrfToken } = await data.json();
-  return csrfToken;
+export const getUserById = async (userId) => {
+  const responseObject = getRequestObject();
+  try {
+    const userResponse = await fetch(`${baseURL}/api/users/${userId}`, responseObject);
+    const user = await userResponse.json();
+    return user;
+  } catch (err) {
+    return err;
+  }
 };
 
-// to use in the future 😀
-export const deleteUser = async (id) => {
-  return await fetch(`${baseURL}/deleteuser/${id}`)
-    .then((data) => data.json())
-    .then((data) => console.log(data));
+export const getQuizzes = async () => {
+  const responseObject = getRequestObject();
+  const quizzes = await fetch(`${baseURL}/api/quizzes`, responseObject);
+  return await quizzes.json();
+};
+
+export const getUserPhoto = async (userId) => {
+  const userPhotoLink = await firebaseStorage.ref(`usersPhotos/${userId}`).getDownloadURL();
+  return userPhotoLink;
 };
