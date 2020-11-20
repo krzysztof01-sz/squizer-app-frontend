b526ed3c9d73d0442aee586af38eac7cda6889e0
Krzysztof <krzysztofs012@outlook.com>
2020-11-20 22:08:38 +0100

    Added new routes: Dashboard, QuizGame and QuizForm

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/App.scss';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import './styles/App.scss';
 import LoginForm from './pages/LoginForm';
 import SignupForm from './pages/SignupForm/index';
 import SplashScreen from './pages/SplashScreen/index';
+import Dashboard from './pages/Dashboard';
+import QuizGame from './pages/Quiz_Game';
+import QuizForm from './pages/QuizForm';
 
 const wrapper = document.querySelector('.app');
 
@@ -20,6 +23,15 @@ const App = () => {
         </Route>
         <Route path="/login">
           <LoginForm />
+        </Route>
+        <Route path="/dashboard">
+          <Dashboard />
+        </Route>
+        <Route path="/quiz/:quizId/play">
+          <QuizGame />
+        </Route>
+        <Route path="/quizform">
+          <QuizForm />
         </Route>
       </Router>
     </React.StrictMode>
