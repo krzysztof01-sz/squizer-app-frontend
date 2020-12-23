68e75afc4f59aaac4e401e42a5e3b770b7f87800
Krzysztof <krzysztofs012@outlook.com>
2020-12-23 21:05:11 +0100

    Made a view when filtering quizzes give no results

---
+++ src/pages/Dashboard/LackOfQuizzesView/index.js
@@ -0,0 +1,11 @@
+import './styles.scss';
+
+const LackOfQuizzesView = () => {
+  return (
+    <section className="lackOfQuizzes">
+      <p className="lackOfQuizzes__message">There is no such a quiz 🙁</p>
+    </section>
+  );
+};
+
+export default LackOfQuizzesView;
