38bfc2f7d8fd581cac4f7fea5395c9f1c38a25ad
Krzysztof <krzysztofs012@outlook.com>
2020-12-23 21:12:41 +0100

    Improved data fetching and view displaying

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,49 +1,66 @@
-import Header from '../../global/Components/Header';
-import Footer from '../../global/Components/Footer';
-import { useEffect, useState } from 'react';
-import { useHistory } from 'react-router-dom';
-import * as api from '../../api/userAPI';
-import Loader from '../../global/Components/Loader';
+import { useEffect, useState, memo } from 'react';
+import * as api from '../../api/index';
 
 import Nav from './Nav/index';
 import QuizzesList from './QuizzesList/index';
 import SearchBar from './SearchBar/index';
 import DashboardHeader from './DashboardHeader/index';
+import ErrorPage from '../ErrorPage';
+import Layout from '../../global/Components/Layout/index';
+import Loader from '../../global/Components/Loader/index';
+import { SearchIcon } from '../../global/Icons/index';
 
-import '../../styles/pages/Dashboard/index.scss';
+import './index.scss';
+import { responseTypes } from '../../utils/constants';
+import FilteringProvider from '../../contexts/Filtering';
 
 const Dashboard = () => {
-  const history = useHistory();
-  const [user, setUser] = useState(null);
   const [quizzes, setQuizzes] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(async () => {
-    const isLogged = async () => {
-      const [user] = await api.knockTo('dashboard');
-      if (!user._id) return setTimeout(() => history.push('/login'), 800);
-
-      setUser(user);
-      const quizzes = await api.getQuizzes();
-      console.log(quizzes);
-      setQuizzes(quizzes);
+    const getQuizzes = async () => {
+      const response = await api.getQuizzes();
+      const { type } = response;
+      if (type === responseTypes.success) {
+        const { quizzes } = response;
+        setQuizzes(quizzes);
+      } else {
+        const { msg } = response;
+        setError(msg);
+      }
+      setLoading(false);
     };
-    isLogged();
+    getQuizzes();
   }, []);
 
-  return user && quizzes ? (
-    <>
-      <Header isLogged={user} />
-      <section className="navSearchbarWrapper">
-        <Nav />
-        <SearchBar />
-      </section>
-      <DashboardHeader />
-      <QuizzesList quizzes={quizzes} />
-      <Footer />
-    </>
-  ) : (
-    <Loader />
+  if (loading) return <Loader />;
+  if (error) return <ErrorPage msg={error} />;
+
+  return (
+    <Layout>
+      <FilteringProvider>
+        <section className="navSearchbarWrapper">
+          <Nav />
+          <SearchBar>
+            {(filterQuizzes) => {
+              return (
+                <div className="searchBarWrapper">
+                  <label className="label">
+                    <input type="text" className="input searchBar" placeholder="search..." onChange={(e) => filterQuizzes(e)} />
+                    <SearchIcon />
+                  </label>
+                </div>
+              );
+            }}
+          </SearchBar>
+        </section>
+        <DashboardHeader />
+        <QuizzesList quizzes={quizzes} />
+      </FilteringProvider>
+    </Layout>
   );
 };
 
-export default Dashboard;
+export default memo(Dashboard);
