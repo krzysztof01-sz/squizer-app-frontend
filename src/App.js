e6a965ad197f87bc2c0f3f1bd7b49d127031f89a
Krzysztof <krzysztofs012@outlook.com>
2020-12-23 21:01:34 +0100

    Made protected routes

--- src/App.js
+++ src/App.js
@@ -1,41 +1,45 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 import './styles/App.scss';
 import LoginForm from './pages/LoginForm';
 import SignupForm from './pages/SignupForm/index';
 import SplashScreen from './pages/SplashScreen/index';
 import Dashboard from './pages/Dashboard';
-import QuizGame from './pages/Quiz_Game';
+import QuizGame from './pages/QuizGame';
 import QuizForm from './pages/QuizForm';
+import AuthProvider, { AuthContext } from './contexts/Auth';
+import ErrorPage from './pages/ErrorPage';
+import * as fb from './utils/feedbackMessages';
 
 const wrapper = document.querySelector('.app');
 
 const App = () => {
+  const { isLogged } = useContext(AuthContext);
   return (
     <React.StrictMode>
       <Router>
-        <Route exact path="/">
-          <SplashScreen />
-        </Route>
-        <Route path="/signup">
-          <SignupForm />
-        </Route>
-        <Route path="/login">
-          <LoginForm />
-        </Route>
-        <Route path="/dashboard">
-          <Dashboard />
-        </Route>
-        <Route path="/quiz/:quizId/play">
-          <QuizGame />
-        </Route>
-        <Route path="/quizform">
-          <QuizForm />
-        </Route>
+        <Switch>
+          <Route exact path="/">
+            {isLogged ? <Redirect to="/dashboard" /> : <SplashScreen />}
+          </Route>
+          <Route path="/signup">{isLogged ? <Redirect to="/dashboard" /> : <SignupForm />}</Route>
+          <Route path="/login">{isLogged ? <Redirect to="/dashboard" /> : <LoginForm />}</Route>
+          <Route path="/dashboard">{isLogged ? <Dashboard /> : <Redirect to="/login" />}</Route>
+          <Route path="/quiz/:quizId/play">{isLogged ? <QuizGame /> : <Redirect to="/login" />}</Route>
+          <Route path="/quizform">{isLogged ? <QuizForm /> : <Redirect to="/login" />}</Route>
+          <Route>
+            <ErrorPage msg={fb.PAGE_DOESNT_EXISTS} />
+          </Route>
+        </Switch>
       </Router>
     </React.StrictMode>
   );
 };
 
-ReactDOM.render(<App />, wrapper);
+ReactDOM.render(
+  <AuthProvider>
+    <App />
+  </AuthProvider>,
+  wrapper,
+);
