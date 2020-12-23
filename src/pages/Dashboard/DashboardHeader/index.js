12986a4c5777824e08129a737219c2c5464361e2
Krzysztof <krzysztofs012@outlook.com>
2020-12-23 21:02:30 +0100

    Added an icon to the dashboard header and put a styles in directory

--- src/pages/Dashboard/DashboardHeader/index.js
+++ src/pages/Dashboard/DashboardHeader/index.js
@@ -1,11 +1,14 @@
-import { PlusIcon } from '../../../global/Icons/icons';
+import { Link } from 'react-router-dom';
+import { PlusIcon } from '../../../global/Icons/index';
 import './styles.scss';
 
 const DashboardHeader = () => {
   return (
     <header className="dashboardHeader">
       <p className="dashboardHeader__title">Quizzes</p>
-      <PlusIcon />
+      <Link to="/quizform">
+        <PlusIcon />
+      </Link>
     </header>
   );
 };
