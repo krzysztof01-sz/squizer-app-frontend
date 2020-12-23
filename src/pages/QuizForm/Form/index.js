d2bdebe40c6483968df48fa221c5e506557ca637
Krzysztof <krzysztofs012@outlook.com>
2020-12-23 21:22:34 +0100

    Added new functions, improved logic and reduced Input component props

--- src/pages/QuizForm/Form/index.js
+++ src/pages/QuizForm/Form/index.js
@@ -1,8 +1,8 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
-import { addQuiz } from '../../../api/userAPI';
+import * as api from '../../../api';
 
-import AddQuiz from '../Buttons/AddQuiz';
+import AddQuizButton from '../Buttons/AddQuiz';
 import AddQuestionButton from '../Buttons/AddQuestion';
 import PreviousQuestionButton from '../Buttons/PreviousQuestion';
 
@@ -10,13 +10,15 @@ import Input from '../Inputs/Text';
 import SelectInput from '../Inputs/Select';
 import TextAreaInput from '../Inputs/Textarea';
 
-import FeedbackMessage from '../../../global/Components/FeedbackMessage/index';
+import * as fb from '../../../utils/feedbackMessages';
+import ErrorMessage from '../../../global/Components/Messages/ErrorMessage';
+import ActionResultMessage from '../../../global/Components/Messages/ActionResultMessage';
+
+import ErrorPage from '../../ErrorPage';
+import Layout from '../../../global/Components/Layout';
+import { categories, responseTypes } from '../../../utils/constants';
 
 const Form = () => {
-  const initialQuestionID = 0;
-  const [messages, setMessages] = useState([]);
-  const [isSubmitting, setIsSubmitting] = useState(false);
-  const history = useHistory();
   const [generalQuizData, setGeneralQuizData] = useState({
     title: '',
     category: '',
@@ -29,6 +31,11 @@ const Form = () => {
     optionalAnswer2: '',
     optionalAnswer3: '',
   };
+  const initialQuestionID = 0;
+  const [messages, setMessages] = useState([]);
+  const [error, setError] = useState(null);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const history = useHistory();
   const [questionID, setQuestionID] = useState(initialQuestionID);
   const [questions, setQuestions] = useState([{ ...initialQuestionData }]);
   const [errors, setErrors] = useState([]);
@@ -72,7 +79,7 @@ const Form = () => {
     return allFieldsFilled;
   };
 
-  const validateQuestion = (question) => {
+  const isQuestionValid = (question) => {
     const questionCopy = { ...question };
     const questionUnique = isQuestionUnique(questionCopy.content);
     const allFieldsFilled = areAllFieldsFilled(questionCopy);
@@ -83,9 +90,9 @@ const Form = () => {
     const answersUnique = areAnswersUnique(questionAnswers);
     const messages = [];
 
-    if (!questionUnique) messages.push('The question was repeated');
-    if (!allFieldsFilled) messages.push('You did not fill all the fields');
-    if (!answersUnique) messages.push('The answers are not unique');
+    if (!questionUnique) messages.push(fb.QUESTION_REPEATED);
+    if (!allFieldsFilled) messages.push(fb.NOT_ALL_FILLED);
+    if (!answersUnique) messages.push(fb.NOT_UNIQUE_ANSWERS);
 
     return messages.length === 0 ? true : messages;
   };
@@ -108,163 +115,176 @@ const Form = () => {
 
       setErrors([]);
       const structuredQuestions = [];
-      questionsWithTrimmedFields.forEach(
-        ({ content, correctAnswer, optionalAnswer1, optionalAnswer2, optionalAnswer3 }) => {
-          const correctAnswerID = getRandomNumber();
-          structuredQuestions.push({
-            content: content,
-            answers: [
-              { letter: correctAnswerID, content: correctAnswer },
-              { letter: getRandomNumber(), content: optionalAnswer1 },
-              { letter: getRandomNumber(), content: optionalAnswer2 },
-              { letter: getRandomNumber(), content: optionalAnswer3 },
-            ],
-            correctAnswer: correctAnswerID,
-          });
-        },
-      );
+      questionsWithTrimmedFields.forEach(({ content, correctAnswer, optionalAnswer1, optionalAnswer2, optionalAnswer3 }) => {
+        const correctAnswerID = getRandomNumber();
+        structuredQuestions.push({
+          content: content,
+          answers: [
+            { answerId: correctAnswerID, content: correctAnswer },
+            { answerId: getRandomNumber(), content: optionalAnswer1 },
+            { answerId: getRandomNumber(), content: optionalAnswer2 },
+            { answerId: getRandomNumber(), content: optionalAnswer3 },
+          ],
+          correctAnswer: correctAnswerID,
+        });
+      });
       structuredQuestions.pop();
       const quiz = { ...generalData, questions: [...structuredQuestions] };
       return quiz;
-    } else return setErrors(['Fill all data about the quiz']);
+    } else return setErrors([fb.FILL_ALL_DATA]);
   };
 
+  const focusOnFirstInput = () => document.querySelector('input[name="content"]').focus();
+
+  const userCanAddAQuiz = () => {
+    const thereAreAtLeast2QuestionsSet = questionID >= 2;
+    const userIsFillingTheNewestQuestion = !questions[questionID + 1];
+
+    return thereAreAtLeast2QuestionsSet && userIsFillingTheNewestQuestion ? true : false;
+  };
+
+  if (error) return <ErrorPage msg={error} />;
+
+  const currentQuestion = questions[questionID];
+
   return (
-    <form method="POST" className="form__wrapper quizForm__wrapper">
-      <header className="form__header quizForm__header">Quiz creator</header>
-      <section className="form quizForm">
-        <section className="quizDataPart">
-          <Input
-            name="title"
-            labelName="title"
-            value={generalQuizData?.title}
-            callback={({ target }) => setGeneralQuizData({ ...generalQuizData, [target.name]: target.value })}
-          />
+    <Layout>
+      <form method="POST" className="form__wrapper quizForm__wrapper">
+        <header className="form__header quizForm__header">Quiz creator</header>
+        <section className="form quizForm">
+          <section className="quizDataPart">
+            <Input
+              name="title"
+              labelName="title"
+              value={generalQuizData?.title}
+              callback={({ target }) => setGeneralQuizData({ ...generalQuizData, [target.name]: target.value })}
+            />
 
-          <SelectInput
-            labelName="category"
-            inputText="Choose a category"
-            name="category"
-            options={['maths', 'it', 'english', 'riddles']}
-            value={generalQuizData?.category}
-            callback={({ target }) => setGeneralQuizData({ ...generalQuizData, [target.name]: target.value })}
-          />
+            <SelectInput
+              labelName="category"
+              inputText="Choose a category"
+              name="category"
+              options={categories}
+              value={generalQuizData?.category}
+              callback={({ target }) => setGeneralQuizData({ ...generalQuizData, [target.name]: target.value })}
+            />
 
-          <TextAreaInput
-            labelName="description"
-            name="description"
-            rows="5"
-            cols="40"
-            value={generalQuizData?.description}
-            callback={({ target }) => setGeneralQuizData({ ...generalQuizData, [target.name]: target.value })}
-          />
-        </section>
+            <TextAreaInput
+              labelName="description"
+              name="description"
+              rows="5"
+              cols="40"
+              value={generalQuizData?.description}
+              callback={({ target }) => setGeneralQuizData({ ...generalQuizData, [target.name]: target.value })}
+            />
+          </section>
 
-        <section className="questionsPart">
-          <Input
-            name="content"
-            labelName="question"
-            value={questions[questionID]?.content}
-            callback={({ target: { value, name } }) => setQuestionData(value, name)}
-          />
-          <Input
-            name="correctAnswer"
-            labelName="correct answer"
-            value={questions[questionID]?.correctAnswer}
-            callback={({ target: { value, name } }) => setQuestionData(value, name)}
-          />
-          <Input
-            name="optionalAnswer1"
-            labelName="1st optional answer"
-            value={questions[questionID]?.optionalAnswer1}
-            callback={({ target: { value, name } }) => setQuestionData(value, name)}
-          />
+          <section className="questionsPart">
+            <Input
+              name="content"
+              labelName="question"
+              value={currentQuestion?.content}
+              callback={({ target: { value, name } }) => setQuestionData(value, name)}
+            />
+            <Input
+              name="correctAnswer"
+              labelName="correct answer"
+              value={currentQuestion?.correctAnswer}
+              callback={({ target: { value, name } }) => setQuestionData(value, name)}
+            />
+            <Input
+              name="optionalAnswer1"
+              labelName="1st optional answer"
+              value={currentQuestion?.optionalAnswer1}
+              callback={({ target: { value, name } }) => setQuestionData(value, name)}
+            />
 
-          <Input
-            name="optionalAnswer2"
-            labelName="2nd optional answer"
-            value={questions[questionID]?.optionalAnswer2}
-            callback={({ target: { value, name } }) => setQuestionData(value, name)}
-          />
+            <Input
+              name="optionalAnswer2"
+              labelName="2nd optional answer"
+              value={currentQuestion?.optionalAnswer2}
+              callback={({ target: { value, name } }) => setQuestionData(value, name)}
+            />
 
-          <Input
-            name="optionalAnswer3"
-            labelName="3rd optional answer"
-            value={questions[questionID]?.optionalAnswer3}
-            callback={({ target: { value, name } }) => setQuestionData(value, name)}
-          />
+            <Input
+              name="optionalAnswer3"
+              labelName="3rd optional answer"
+              value={currentQuestion?.optionalAnswer3}
+              callback={({ target: { value, name } }) => setQuestionData(value, name)}
+            />
 
-          <p className="questionNumber">question no. {questionID + 1}</p>
+            <p className="questionNumber">question no. {questionID + 1}</p>
 
-          {errors.map((error, index) => {
-            return <FeedbackMessage key={index} message={error} type="error" />;
-          })}
+            {errors.map((error, index) => {
+              return <ErrorMessage key={index} message={error} />;
+            })}
 
-          {messages.map(({ msg, type }, index) => {
-            return <FeedbackMessage key={index} message={msg} type={type} />;
-          })}
+            {messages.map(({ msg, type }, index) => {
+              return <ActionResultMessage key={index} msg={msg} type={type} />;
+            })}
 
-          <section className="questionsNavigation">
-            <div className="buttonWrapper">
-              {questionID >= 1 ? (
-                <PreviousQuestionButton
+            <section className="questionsNavigation">
+              <div className="buttonWrapper">
+                {questionID >= 1 ? (
+                  <PreviousQuestionButton
+                    callback={(e) => {
+                      e.preventDefault();
+                      setQuestionID(questionID - 1);
+                    }}
+                  />
+                ) : null}
+              </div>
+
+              <div className="buttonWrapper">
+                <AddQuestionButton
                   callback={(e) => {
                     e.preventDefault();
-                    if (questionID >= 1) {
-                      setQuestionID(questionID - 1);
+                    setErrors([]);
+                    if (questionID < questions.length) {
+                      const validationResult = isQuestionValid(currentQuestion);
+
+                      if (typeof validationResult === 'boolean') {
+                        const currentQuestionIsDisplayed = questions[questionID + 1] ? false : true;
+                        if (currentQuestionIsDisplayed) {
+                          setQuestions(() => [...questions, initialQuestionData]);
+                        }
+                      } else {
+                        focusOnFirstInput();
+                        return setErrors(validationResult);
+                      }
                     }
+                    setQuestionID(questionID + 1);
+                    focusOnFirstInput();
+                  }}
+                />
+              </div>
+            </section>
+            <aside className="buttonWrapper">
+              {userCanAddAQuiz() ? (
+                <AddQuizButton
+                  isActive={isSubmitting}
+                  callback={async () => {
+                    setIsSubmitting(true);
+                    const quiz = prepareQuestionStructure();
+                    let { type, msg } = await api.addQuiz(quiz);
+
+                    // when token is invalid, middleware returns plain text, so here I preserve it
+                    if (type === responseTypes.error && typeof msg === 'string') {
+                      setError(msg);
+                    } else {
+                      setMessages(msg);
+                    }
+
+                    if (type === responseTypes.success) return history.push('/dashboard');
+                    setIsSubmitting(false);
                   }}
                 />
               ) : null}
-            </div>
-
-            <div className="buttonWrapper">
-              <AddQuestionButton
-                callback={(e) => {
-                  e.preventDefault();
-                  setErrors([]);
-                  if (questionID < questions.length) {
-                    const validationResult = validateQuestion(questions[questionID]);
-
-                    if (typeof validationResult === 'object') {
-                      return setErrors(validationResult);
-                    } else {
-                      const currentQuestionIsDisplayed = questions[questionID + 1] ? false : true;
-                      if (currentQuestionIsDisplayed) {
-                        setQuestions(() => [...questions, initialQuestionData]);
-                      }
-                    }
-                  }
-                  setQuestionID(questionID + 1);
-                  document.querySelector('input[name="content"]').focus();
-                }}
-              />
-            </div>
+            </aside>
           </section>
-          <aside className="buttonWrapper">
-            {questionID >= 2 && !questions[questionID + 1] ? (
-              <AddQuiz
-                isActive={isSubmitting}
-                callback={async () => {
-                  setIsSubmitting(true);
-                  const quiz = prepareQuestionStructure();
-                  const response = await addQuiz(quiz);
-                  const responseType = response[0].type;
-
-                  setMessages(response);
-
-                  if (responseType === 'success') {
-                    return history.push('/dashboard');
-                  }
-
-                  setIsSubmitting(false);
-                }}
-              />
-            ) : null}
-          </aside>
         </section>
-      </section>
-    </form>
+      </form>
+    </Layout>
   );
 };
 
--- src/pages/QuizForm/index.js
+++ src/pages/QuizForm/index.js
@@ -1,17 +1,8 @@
-import Header from '../../global/Components/Header/index';
-import Footer from '../../global/Components/Footer/index';
 import Form from './Form/index';
-
 import './index.scss';
 
 const QuizForm = () => {
-  return (
-    <>
-      <Header />
-      <Form />
-      <Footer />
-    </>
-  );
+  return <Form />;
 };
 
 export default QuizForm;
