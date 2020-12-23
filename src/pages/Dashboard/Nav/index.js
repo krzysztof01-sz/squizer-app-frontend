c634a0b0debedd2c459082fefd0a8391a4cfecd6
Krzysztof <krzysztofs012@outlook.com>
2020-12-23 21:05:53 +0100

    Put an link to the quizform page

--- src/pages/Dashboard/Nav/index.js
+++ src/pages/Dashboard/Nav/index.js
@@ -1,17 +1,20 @@
 import { useState } from 'react';
-
-import { ChevronIcon } from '../../../global/Icons/icons';
+import { Link } from 'react-router-dom';
+import { ChevronIcon } from '../../../global/Icons/index';
 import './styles.scss';
 
 const Nav = () => {
   const [isOpen, setIsOpen] = useState(false);
+
   return (
     <nav className="nav">
       <header onClick={() => setIsOpen(!isOpen)} className="nav__header">
         menu <ChevronIcon />
       </header>
       <ul className="nav__list" style={{ display: `${isOpen ? 'block' : 'none'}` }}>
-        <li className="nav__item">add a new quiz</li>
+        <Link to="/quizform">
+          <li className="nav__item">add a new quiz</li>
+        </Link>
         <li className="nav__item">user profile</li>
         <li className="nav__item">about</li>
       </ul>
