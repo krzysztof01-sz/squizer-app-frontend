8d374dfb472d569091a6e6edd5882639b1b339c1
Krzysztof <krzysztofs012@outlook.com>
2020-12-23 21:16:29 +0100

    Added reusable error page

---
+++ src/pages/ErrorPage/index.js
@@ -0,0 +1,23 @@
+import Layout from '../../global/Components/Layout';
+import Nav from '../Dashboard/Nav';
+
+import './styles.scss';
+
+const ErrorPage = ({ msg, children }) => {
+  return (
+    <Layout>
+      <Nav />
+      <section className="errorPage">
+        <p className="errorPage__message">
+          {msg}
+          <span role="img" aria-label="sad emoji">
+            🥺
+          </span>
+        </p>
+        {children}
+      </section>
+    </Layout>
+  );
+};
+
+export default ErrorPage;
