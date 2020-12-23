50916eccd5bb539741776623dc77757887c67f1d
Krzysztof <krzysztofs012@outlook.com>
2020-12-23 21:32:47 +0100

    Removed unnecessary files

--- src/pages/Quiz_Game/QuestionDot/index.js
+++
@@ -1,7 +0,0 @@
-import './styles.scss';
-
-const QuestionDot = ({ setQuestionID, filled }) => {
-  return <div onClick={setQuestionID} className={`${filled ? 'dot--filled' : 'dot'}`}></div>;
-};
-
-export default QuestionDot;
