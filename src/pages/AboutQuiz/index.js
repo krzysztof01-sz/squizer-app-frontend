aa71a271d35c90291f78a989560a497ad8ca2205
Krzysztof <krzysztofs012@outlook.com>
2021-02-04 22:48:12 +0100

    Created and styled the AboutQuiz page

---
+++ src/pages/AboutQuiz/index.js
@@ -0,0 +1,32 @@
+import { useParams } from 'react-router-dom';
+import { useQuizCard, useQuizComments } from '../../hooks';
+import Layout from '../../global/Components/Layout';
+import PresentationCard from './PresentationCard';
+import CommentForm from './CommentForm';
+import CommentsList from './CommentsList';
+import Loader from '../../global/Components/Loader';
+import './styles.scss';
+import ErrorPage from '../ErrorPage';
+
+const AboutQuiz = () => {
+  const { quizId } = useParams();
+  const { comments, error: commentError, loading: commentsLoading } = useQuizComments(quizId);
+  const { quiz, user, categoryImage, error: cardError, loading: cardLoading } = useQuizCard(quizId);
+
+  if (commentError) return <ErrorPage msg={commentError} />;
+  if (cardError) return <ErrorPage msg={cardError} />;
+  if (commentsLoading || cardLoading) return <Loader />;
+
+  return (
+    <Layout>
+      <PresentationCard card={{ quiz, user, categoryImage }} />
+
+      <section className="commentsView">
+        <CommentForm />
+        <CommentsList comments={comments} />
+      </section>
+    </Layout>
+  );
+};
+
+export default AboutQuiz;
