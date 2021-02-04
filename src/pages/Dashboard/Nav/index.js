d5fc5003fa907abac1750a0dcab2b257a1cf58bd
Krzysztof <krzysztofs012@outlook.com>
2021-02-04 22:49:07 +0100

    Added an icon to the Nav component

--- src/pages/Dashboard/Nav/index.js
+++ src/pages/Dashboard/Nav/index.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { ChevronIcon } from '../../../global/Icons/index';
+import { ChevronIcon } from '../../../global/Icons';
 import './styles.scss';
 
 const Nav = () => {
