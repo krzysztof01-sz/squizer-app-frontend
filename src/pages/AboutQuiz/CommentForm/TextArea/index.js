2091785b7bd57fa5734fdcef94dcbfbdb66512b1
Krzysztof <krzysztofs012@outlook.com>
2021-02-04 22:45:38 +0100

    Created and styled the Textarea component

---
+++ src/pages/AboutQuiz/CommentForm/TextArea/index.js
@@ -0,0 +1,18 @@
+import './styles.scss';
+
+const TextArea = ({ comment, setComment }) => {
+  return (
+    <label className="label">
+      write a comment
+      <textarea
+        name="content"
+        value={comment}
+        onChange={(e) => setComment(e.target.value)}
+        rows={5}
+        className="input commentForm__input"
+      />
+    </label>
+  );
+};
+
+export default TextArea;
