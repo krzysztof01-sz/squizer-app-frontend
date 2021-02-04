1cb2052ae2b2ce2253e35d7ff59cfef0e3fc8221
Krzysztof <krzysztofs012@outlook.com>
2021-02-04 22:43:18 +0100

    Added new hooks for aboutQuizPage and refactored others: useQuestion, useQuizzes, useQuizCard and useQuizComments

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,27 +1,15 @@
 import { useEffect, useState } from 'react';
-import * as api from '../api';
 import { responseTypes } from '../utils/constants';
-import DefaultPhoto from '../assets/images/DefaultPhoto.png';
+import { shuffleArray } from '../utils/functions';
+import * as api from '../api';
 
 export const useUser = (id) => {
   const [user, setUser] = useState(null);
 
   useEffect(() => {
     const getUserData = async (userId) => {
-      const response = await api.getUserById(userId);
-      const { type } = response;
-      if (type === responseTypes.success) {
-        const { user } = response;
-        delete user.password;
-        setUser(user);
-        if (user.photoType === 'custom') {
-          const photoLink = await api.getUserPhoto(userId);
-          setUser((prev) => ({ ...prev, userPhoto: photoLink }));
-        } else setUser((prev) => ({ ...prev, userPhoto: DefaultPhoto }));
-      } else {
-        setUser((prev) => ({ ...prev, nick: '' }));
-        setUser((prev) => ({ ...prev, userPhoto: DefaultPhoto }));
-      }
+      const user = await api.getUserById(userId);
+      setUser(user);
     };
     getUserData(id);
   }, []);
@@ -30,15 +18,126 @@ export const useUser = (id) => {
 };
 
 export const useCategoryImage = (category) => {
+  if (!category) return null;
   const [categoryImage, setCategoryImage] = useState('');
 
   useEffect(() => {
-    const getCategoryImage = async (moduleName) => {
-      const { default: image } = await import(`../assets/images/categories/${moduleName}.png`);
+    const getImage = async (cat) => {
+      const image = await api.getCategoryImage(cat);
       setCategoryImage(image);
     };
-    getCategoryImage(category);
+    getImage(category);
   }, []);
 
   return { categoryImage };
+};
+
+export const useQuestions = (quizId) => {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    const getQuestions = async () => {
+      const response = await api.getQuizQuestions(quizId);
+
+      const { type } = response;
+      if (type === responseTypes.success) {
+        const { questions } = response;
+        questions.map((question) => shuffleArray(question.answers));
+        setQuestions(questions);
+      } else {
+        const { msg } = response;
+        setError(msg);
+      }
+      setLoading(false);
+    };
+    getQuestions();
+  }, []);
+
+  return { questions, loading, error };
+};
+
+export const useQuizzes = () => {
+  const [quizzes, setQuizzes] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(async () => {
+    const getQuizzes = async () => {
+      const response = await api.getQuizzes();
+      const { type } = response;
+      if (type === responseTypes.success) {
+        const { quizzes } = response;
+        setQuizzes(quizzes);
+      } else {
+        const { msg } = response;
+        setError(msg);
+      }
+      setLoading(false);
+    };
+    getQuizzes();
+  }, []);
+
+  return { quizzes, loading, error };
+};
+
+export const useQuizCard = (quizId) => {
+  const [quiz, setQuiz] = useState(null);
+  const [user, setUser] = useState(null);
+  const [categoryImage, setCategoryImage] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const getQuiz = async (id) => {
+      try {
+        const response = await api.getQuiz(id);
+        const { type } = response;
+
+        if (type === responseTypes.success) {
+          setQuiz(response.data);
+        } else {
+          throw response.msg;
+        }
+
+        const { createdBy, category } = response.data;
+        const user = await api.getUserById(createdBy);
+        const categoryImage = await api.getCategoryImage(category);
+        setUser(user);
+        setCategoryImage(categoryImage);
+      } catch (e) {
+        setError(e);
+      }
+
+      setLoading(false);
+    };
+    getQuiz(quizId);
+
+    return () => setLoading(false);
+  }, []);
+
+  return { quiz, user, categoryImage, error, loading };
+};
+
+export const useQuizComments = (quizId) => {
+  const [comments, setComments] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const getQuizComments = async (id) => {
+      setLoading(true);
+      const response = await api.getQuizComments(id);
+      if (response.type === responseTypes.success) {
+        setComments(response.comments);
+      } else {
+        setError(response.msg);
+      }
+      setLoading(false);
+    };
+    getQuizComments(quizId);
+  }, []);
+
+  return { comments, error, loading };
 };
