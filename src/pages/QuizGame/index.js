19551e888bd2a7909c7b635ec5866b8a4c1e74e0
Krzysztof <krzysztofs012@outlook.com>
2021-02-04 22:58:41 +0100

    Refactored QuizGame page

--- src/pages/QuizGame/index.js
+++ src/pages/QuizGame/index.js
@@ -1,11 +1,10 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import * as api from '../../api/index';
 
-import Answer from './Answer/index';
-import QuestionContent from './QuestionContent/index';
-import SwitchDot from './SwitchDot/index';
-import QuizResult from './QuizResult/index';
+import Answer from './Answer';
+import QuestionContent from './QuestionContent';
+import SwitchDot from './SwitchDot';
+import QuizResult from './QuizResult';
 import Layout from '../../global/Components/Layout';
 import Loader from '../../global/Components/Loader';
 import ErrorPage from '../ErrorPage';
@@ -13,42 +12,17 @@ import ErrorPage from '../ErrorPage';
 import PreviousQuestionButton from './Buttons/PreviousQuestion';
 import NextQuestionButton from './Buttons/NextQuestion';
 import FinishQuizButton from './Buttons/FinishQuiz';
-
-import { responseTypes } from '../../utils/constants';
+import { useQuestions } from '../../hooks';
 import './index.scss';
 
 const QuizGame = () => {
   const { quizId } = useParams();
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
   const [userAnswers, setUserAnswers] = useState([]);
-  const [questions, setQuestions] = useState([]);
   const [questionID, setQuestionID] = useState(0);
   const [correctUserAnswersQuantity, setCorrectUserAnswersQuantity] = useState(false);
+  const { questions, loading, error } = useQuestions(quizId);
 
-  useEffect(() => {
-    const getQuestions = async () => {
-      const response = await api.getQuizQuestions(quizId);
-
-      const { type } = response;
-      if (type === responseTypes.success) {
-        const { questions } = response;
-        questions.map((question) => shuffleArray(question.answers));
-        setQuestions(questions);
-        setUserAnswers(new Array(questions.length).fill(undefined));
-      } else {
-        const { msg } = response;
-        setError(msg);
-      }
-      setLoading(false);
-    };
-    getQuestions();
-  }, []);
-
-  const shuffleArray = (arr) => {
-    arr = arr.sort(() => Math.random() - 0.5);
-    return arr;
-  };
+  useEffect(() => setUserAnswers(new Array(questions.length).fill(undefined)), [loading]);
 
   const getCorrectAnswers = () => {
     const correctAnswers = [];
@@ -72,7 +46,13 @@ const QuizGame = () => {
   const renderSwitchDots = () => {
     const questionsDots = [];
     for (let i = 0; i < questions.length; i++) {
-      const element = <SwitchDot filled={i === questionID ? true : false} key={i} callback={() => setQuestionID(i)} />;
+      const element = (
+        <SwitchDot
+          filled={i === questionID ? true : false}
+          key={i}
+          callback={() => setQuestionID(i)}
+        />
+      );
       questionsDots.push(element);
     }
     return questionsDots;
@@ -126,7 +106,9 @@ const QuizGame = () => {
         </section>
         <section className="gameNavigationButtons">
           <div className="gameNavigation__left">
-            {questionID > 0 ? <PreviousQuestionButton setQuestion={() => setQuestionID(questionID - 1)} /> : null}
+            {questionID > 0 ? (
+              <PreviousQuestionButton setQuestion={() => setQuestionID(questionID - 1)} />
+            ) : null}
           </div>
           <div className="gameNavigation__right">
             {questionID < questions.length - 1 ? (
@@ -146,7 +128,11 @@ const QuizGame = () => {
   } else
     return (
       <Layout>
-        <QuizResult userAnswers={userAnswers} questions={questions} correctUserAnswersQuantity={correctUserAnswersQuantity} />
+        <QuizResult
+          userAnswers={userAnswers}
+          questions={questions}
+          correctUserAnswersQuantity={correctUserAnswersQuantity}
+        />
       </Layout>
     );
 };
