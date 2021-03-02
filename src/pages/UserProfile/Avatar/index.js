d3700dc4ae205accd464e6b3f6bdd0192c70d0d5
Krzysztof <krzysztofs012@outlook.com>
2021-03-02 23:14:37 +0100

    Added dynamic content from DB + styling improvements

--- src/pages/UserProfile/Avatar/index.js
+++ src/pages/UserProfile/Avatar/index.js
@@ -1,7 +1,18 @@
+import Loader from '../../../global/Components/Loader';
+import { useUser } from '../../../hooks';
 import './styles.scss';
 
-const Avatar = ({ src }) => {
-  return <img className="profile__avatar" alt="user avatar" src={src} />;
+const Avatar = ({ userId }) => {
+  const { user } = useUser(userId);
+  if (user) {
+    return <img className="profile__avatar" alt="user avatar" src={user.avatar} />;
+  } else {
+    return (
+      <div className="profile__avatar">
+        <Loader />
+      </div>
+    );
+  }
 };
 
 export default Avatar;
