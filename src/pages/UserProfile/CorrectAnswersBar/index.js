5e48793090f268b52a490227daa866d2e96ddc80
Krzysztof <krzysztofs012@outlook.com>
2021-03-02 23:12:28 +0100

    Added animation

--- src/pages/UserProfile/CorrectAnswersBar/index.js
+++ src/pages/UserProfile/CorrectAnswersBar/index.js
@@ -1,9 +1,19 @@
+import { useEffect } from 'react';
 import './styles.scss';
 
 const CorrectAnswersBar = ({ percent }) => {
+  useEffect(() => {
+    setTimeout(() => {
+      document.querySelector('.correctAnswersBar').classList.add('animate');
+      document.querySelector('.progressBar').classList.add('animate');
+    }, 500);
+  });
+
   return (
     <section className="correctAnswersBar">
-      <div className="progressBar">{percent}% correct answers</div>
+      <div className="progressBar" style={{ width: `${percent}%` }}>
+        <p className="progressBar__percentage">{percent}% correct answers</p>
+      </div>
     </section>
   );
 };
