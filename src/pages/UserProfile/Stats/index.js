4c4d25cca740b91de74e21414fb2e23e9a58de31
Krzysztof <krzysztofs012@outlook.com>
2021-03-02 23:10:50 +0100

    Added content from DB, added formatPlace function

--- src/pages/UserProfile/Stats/index.js
+++ src/pages/UserProfile/Stats/index.js
@@ -1,12 +1,17 @@
 import './styles.scss';
 
-const Stats = ({ rankingPlace, usersQuantity, points }) => {
+const formatPlace = (place) => {
+  if (place === 1) return `${place}st`;
+  if (place === 2) return `${place}nd`;
+  if (place === 3) return `${place}rd`;
+  return `${place}th`;
+};
+
+const Stats = ({ rankingPlace, points }) => {
   return (
     <div className="userData__stats">
-      <p>
-        Ranking: {rankingPlace}/{usersQuantity}
-      </p>
-      <p>{points} points</p>
+      <p>Ranking: {`${formatPlace(rankingPlace)} place`}</p>
+      <p>Scored points: {points}</p>
     </div>
   );
 };
