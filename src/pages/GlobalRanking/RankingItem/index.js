e98f448793fc5e5d254ca285b67aa23999c8ed2e
Krzysztof <krzysztofs012@outlook.com>
2021-03-02 23:20:46 +0100

    Emphasised user result in rankingList

--- src/pages/GlobalRanking/RankingItem/index.js
+++ src/pages/GlobalRanking/RankingItem/index.js
@@ -1,10 +1,12 @@
 import { renderAvatar } from '../helpers/renderAvatar';
 import './styles.scss';
 
-const RankingItem = ({ place, user: { nickname, points, avatar }, loading }) => {
+const RankingItem = ({ place, user: { nickname, points, avatar, isItMe }, loading }) => {
   return (
     <section className="rankingList__item">
-      <span className="item__data">{`${place}. ${nickname} (${points} points)`}</span>
+      <span
+        className={`item__data ${isItMe ? 'item__data--authUser' : ''}`}
+      >{`${place}. ${nickname} (${points} points)`}</span>
       {renderAvatar(loading, avatar)}
     </section>
   );
