76738d7d71d76e65a2e9dc5af1d3c9536835450b
Krzysztof <krzysztofs012@outlook.com>
2021-03-02 23:09:57 +0100

    Added loading content from DB

--- src/pages/UserProfile/UserQuiz/index.js
+++ src/pages/UserProfile/UserQuiz/index.js
@@ -1,19 +1,32 @@
-import { BinIcon, EditIcon } from '../../../global/Icons';
+import { Link } from 'react-router-dom';
+import Loader from '../../../global/Components/Loader';
+import { BinIcon } from '../../../global/Icons';
+import { useQuiz } from '../../../hooks';
+import { shortenText } from '../../../utils/functions';
 import './styles.scss';
 
-const UserQuiz = () => {
+const UserQuiz = ({ quizId }) => {
+  const { quiz, error, loading } = useQuiz(quizId);
+
+  if (loading) return <Loader width={200} height={200} />;
+  if (error)
+    return (
+      <article className="userQuiz">
+        <SectionHeader isCenter={true}>Quiz not found</SectionHeader>
+      </article>
+    );
+
   return (
     <article className="userQuiz">
-      <p className="userQuiz__title">Linear function - advanced</p>
-      <p className="userQuiz__description">
-        Do you really know you does internet and computer networks works?
-      </p>
+      <p className="userQuiz__title">{quiz.title}</p>
+      <p className="userQuiz__description">{shortenText(quiz.description, 200)}</p>
       <section className="userQuiz__actionsBar">
         <div className="iconsWrapper">
-          <EditIcon />
           <BinIcon />
         </div>
-        <button className="button userQuiz__button">check</button>
+        <Link to={`/quiz/${quizId}/about`}>
+          <button className="button userQuiz__button">check</button>
+        </Link>
       </section>
     </article>
   );
