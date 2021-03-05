b99d59644d71b7bf176bc3c8d34b7b79d3379d9c
Krzysztof <krzysztofs012@outlook.com>
2021-03-05 19:41:40 +0100

    Added logic for quiz deleting

--- src/pages/UserProfile/UserQuiz/index.js
+++ src/pages/UserProfile/UserQuiz/index.js
@@ -1,11 +1,17 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Loader from '../../../global/Components/Loader';
-import { BinIcon } from '../../../global/Icons';
 import { useQuiz } from '../../../hooks';
+import { responseTypes } from '../../../utils/constants';
+import { QUIZ_DELETING_CONFIRMATION } from '../../../utils/feedbackMessages';
 import { shortenText } from '../../../utils/functions';
+import DeleteQuizButton from './DeleteQuizButton';
+import ActionResultMessage from '../../../global/Components/Messages/ActionResultMessage';
+import * as api from '../../../api';
 import './styles.scss';
 
 const UserQuiz = ({ quizId }) => {
+  const [deletingResult, setDeletingResult] = useState(null);
   const { quiz, error, loading } = useQuiz(quizId);
 
   if (loading) return <Loader width={200} height={200} />;
@@ -22,12 +28,25 @@ const UserQuiz = ({ quizId }) => {
       <p className="userQuiz__description">{shortenText(quiz.description, 200)}</p>
       <section className="userQuiz__actionsBar">
         <div className="iconsWrapper">
-          <BinIcon />
+          <DeleteQuizButton
+            callback={async () => {
+              if (confirm(QUIZ_DELETING_CONFIRMATION)) {
+                const result = await api.deleteQuiz(quizId);
+                setDeletingResult(result);
+
+                if (result.type === responseTypes.success) {
+                  location.reload();
+                }
+              }
+            }}
+          />
         </div>
         <Link to={`/quiz/${quizId}/about`}>
           <button className="button userQuiz__button">check</button>
         </Link>
       </section>
+
+      <ActionResultMessage msg={deletingResult?.msg} type={deletingResult?.type} />
     </article>
   );
 };
