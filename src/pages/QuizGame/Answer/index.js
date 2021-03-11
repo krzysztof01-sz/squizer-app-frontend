4b76cfac4baf0063dde5398eb8552921b6b3756d
Krzysztof <krzysztofs012@outlook.com>
2021-03-11 12:22:36 +0100

    Added a11y improvements

--- src/pages/QuizGame/Answer/index.js
+++ src/pages/QuizGame/Answer/index.js
@@ -3,11 +3,18 @@ import './styles.scss';
 const Answer = ({ content, setAnswer, letter }) => {
   return (
     <section
+      tabIndex="0"
       className="answer__content"
       name={letter}
       onClick={({ target }) => {
         setAnswer(letter);
         target.classList.add('answer__content--checked');
+      }}
+      onKeyDown={(e) => {
+        if (e.key === 'Enter') {
+          setAnswer(letter);
+          e.target.classList.add('answer__content--checked');
+        }
       }}
     >
       {content}
