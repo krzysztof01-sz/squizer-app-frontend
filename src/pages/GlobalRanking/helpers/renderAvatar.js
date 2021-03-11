7b29a6b2bc77a6a4b87137be5ffb074a749967cf
Krzysztof <krzysztofs012@outlook.com>
2021-03-11 12:16:41 +0100

    Added loader size

--- src/pages/GlobalRanking/helpers/renderAvatar.js
+++ src/pages/GlobalRanking/helpers/renderAvatar.js
@@ -3,7 +3,7 @@ import Loader from '../../../global/Components/Loader';
 export const renderAvatar = (loading, avatar) => {
   return loading ? (
     <div className="data__avatar">
-      <Loader />
+      <Loader width={40} height={40} />
     </div>
   ) : (
     <img className="data__avatar" src={loading ? '' : avatar} alt="user avatar" />
