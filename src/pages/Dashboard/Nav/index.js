d9734794362a1893f5eadcd0635d112b0a6b55f8
Krzysztof <krzysztofs012@outlook.com>
2021-03-11 12:10:17 +0100

    Corrected code readability

--- src/pages/Dashboard/Nav/index.js
+++ src/pages/Dashboard/Nav/index.js
@@ -3,20 +3,22 @@ import { Link } from 'react-router-dom';
 import { ChevronIcon } from '../../../global/Icons';
 import './styles.scss';
 
-const Nav = () => {
+const Nav = ({ navItems }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="nav">
+    <nav className="nav" aria-label="Navigation">
       <header onClick={() => setIsOpen(!isOpen)} className="nav__header">
         menu <ChevronIcon />
       </header>
       <ul className="nav__list" style={{ display: `${isOpen ? 'block' : 'none'}` }}>
-        <Link to="/quizform">
-          <li className="nav__item">add a new quiz</li>
-        </Link>
-        <li className="nav__item">user profile</li>
-        <li className="nav__item">about</li>
+        {navItems.map(({ route, text }) => {
+          return (
+            <Link key={text} to={route}>
+              <li className="nav__item">{text}</li>
+            </Link>
+          );
+        })}
       </ul>
     </nav>
   );
