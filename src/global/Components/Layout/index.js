871eab4d775cd49a590730946b3a79d938c48fd5
Krzysztof <krzysztofs012@outlook.com>
2021-03-11 12:01:23 +0100

    Added MobileNav component

--- src/global/Components/Layout/index.js
+++ src/global/Components/Layout/index.js
@@ -1,13 +1,18 @@
-import { useEffect } from 'react';
+import { useContext, useEffect } from 'react';
+import { AuthContext } from '../../../contexts/Auth';
+import { navItems } from '../../../utils/constants';
 import Footer from '../Footer';
 import Header from '../Header';
+import MobileNav from '../MobileNav';
 
 const Layout = ({ children }) => {
+  const { isLogged } = useContext(AuthContext);
   useEffect(() => window.scrollTo(0, 0), []);
 
   return (
     <>
       <Header />
+      {isLogged ? <MobileNav navItems={navItems} /> : null}
       {children}
       <Footer />
     </>
