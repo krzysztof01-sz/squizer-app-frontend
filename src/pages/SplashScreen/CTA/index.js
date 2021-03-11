d6946525aa85c091e22147b2ccd0dfbe58e9fdb7
Krzysztof <krzysztofs012@outlook.com>
2021-03-11 12:25:26 +0100

    Added subtle UI changes

--- src/pages/SplashScreen/CTA/index.js
+++ src/pages/SplashScreen/CTA/index.js
@@ -4,7 +4,10 @@ import './styles.scss';
 const CTA = () => {
   return (
     <section className="CTA">
-      <p className="CTA__content">Join and extend our community. Create a quiz and check your knowledge by solving the other's ones.</p>
+      <p className="CTA__content">
+        Join and extend our community. Create a quiz and check your knowledge by solving the other's
+        ones.
+      </p>
       <Link to="/signup">
         <button className="button CTA__button">Join now</button>
       </Link>
