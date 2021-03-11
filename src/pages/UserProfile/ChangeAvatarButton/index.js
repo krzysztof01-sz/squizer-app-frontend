db9961e405357ff140826752458c3e68433adbe6
Krzysztof <krzysztofs012@outlook.com>
2021-03-11 12:29:46 +0100

    Added a11y improvements

--- src/pages/UserProfile/ChangeAvatarButton/index.js
+++ src/pages/UserProfile/ChangeAvatarButton/index.js
@@ -3,12 +3,12 @@ import './styles.scss';
 
 const ChangeAvatarButton = ({ onClickCallback }) => {
   return (
-    <div onClick={onClickCallback} className="changeAvatarButton__wrapper">
+    <button onClick={onClickCallback} className="button changeAvatarButton__wrapper">
       <p className="changeAvatarButton">change</p>
-      <span className="changeAvatarButton__icon">
+      <span role="button" className="changeAvatarButton__icon">
         <CameraIcon />
       </span>
-    </div>
+    </button>
   );
 };
 
