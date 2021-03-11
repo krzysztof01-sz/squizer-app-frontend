dcea9874320fee4a9332e511be35b276d49b045d
Krzysztof <krzysztofs012@outlook.com>
2021-03-11 12:12:50 +0100

    Added a11y and performance improvements

--- src/pages/Dashboard/QuizCard/index.js
+++ src/pages/Dashboard/QuizCard/index.js
@@ -4,18 +4,23 @@ import { formatDate, shortenText } from '../../../utils/functions';
 import { useCategoryImage, useUser } from '../../../hooks';
 import Loader from '../../../global/Components/Loader';
 import './styles.scss';
+import { memo } from 'react';
 
 const QuizCard = ({ quiz: { _id, title, description, creationDate, createdBy, category } }) => {
   const { categoryImage } = useCategoryImage(category);
   const { user } = useUser(createdBy);
 
   return user && categoryImage ? (
-    <article className="quizCard" style={{ borderBottom: `10px solid ${map.get(category)}` }}>
+    <article
+      role="listitem"
+      className="quizCard"
+      style={{ borderBottom: `10px solid ${map.get(category)}` }}
+    >
       <div className="quizCard__categoryImageWrapper">
         <img className="quizCard__categoryImage" src={categoryImage} alt={category} />
-        <img className="quizCard__avatar" src={user.avatar} alt={user.nickname} />
+        <img className="quizCard__avatar" src={user.avatar} alt={`${user.nickname} avatar`} />
       </div>
-      <div className="quizCard__info">
+      <div role="contentinfo" className="quizCard__info">
         <p className="quizCard__title">{title}</p>
         <p className="quizCard__description">
           {shortenText(description, maxDescrptionLengthOnCard)}
@@ -29,9 +34,9 @@ const QuizCard = ({ quiz: { _id, title, description, creationDate, createdBy, ca
     </article>
   ) : (
     <article className="quizCard">
-      <Loader width={300} height={300} />
+      <Loader width={200} height={200} />
     </article>
   );
 };
 
-export default QuizCard;
+export default memo(QuizCard);
