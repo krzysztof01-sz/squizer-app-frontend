791bdcd4de4feae67777288529d77b6a3363dc89
Krzysztof <krzysztofs012@outlook.com>
2021-03-11 12:30:14 +0100

    Added subtle semantic and UI changes

--- src/pages/UserProfile/Greeting/index.js
+++ src/pages/UserProfile/Greeting/index.js
@@ -1,7 +1,7 @@
 import './styles.scss';
 
 const Greeting = ({ name }) => {
-  return <h1 className="greeting">Hi, {name}! 👋</h1>;
+  return <h2 className="greeting">Hi, {name}! 👋</h2>;
 };
 
 export default Greeting;
