7a78f9bab7144bd3a202305e623d01f353ab0e39
Krzysztof <krzysztofs012@outlook.com>
2021-03-12 17:52:03 +0100

    Changed the text in CTA and Description section

--- src/pages/SplashScreen/CTA/index.js
+++ src/pages/SplashScreen/CTA/index.js
@@ -5,8 +5,8 @@ const CTA = () => {
   return (
     <section className="CTA">
       <p className="CTA__content">
-        Join and extend our community. Create a quiz and check your knowledge by solving the other's
-        ones.
+        Create a free account within a few seconds and join to our community right now.
+        Entertainment and knowledge are waiting for you. Have fun! 👊
       </p>
       <Link to="/signup">
         <button className="button CTA__button">Join now</button>
--- src/pages/SplashScreen/Description/index.js
+++ src/pages/SplashScreen/Description/index.js
@@ -4,7 +4,10 @@ const Description = () => {
   return (
     <section className="description">
       <h1 className="description__title">Welcome to sQuizer,</h1>
-      <p className="description__content">a web application that stores away for you a base of online quizzes for many different categories.</p>
+      <p className="description__content">
+        a modern platform that stores loads of quizzes from different cathegories for you. Create,
+        play and rival with others having a lot of fun.
+      </p>
     </section>
   );
 };
