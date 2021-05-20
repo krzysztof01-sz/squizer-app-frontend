37691d7c2a06ed74f7727f9255699ecc72d9de52
Krzysztof <krzysztofs012@outlook.com>
2021-05-20 14:53:04 +0200

    Created components and fixed the UI bug

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,55 +1,61 @@
 import Layout from '../../global/Components/Layout';
 import './index.scss';
 
+const Heading = ({ children }) => {
+  return <h2 className="about__heading">{children}</h2>;
+};
+
+const ListItem = ({ children }) => {
+  return <li className="list__item">{children}</li>;
+};
+
 const About = () => {
   return (
     <Layout>
       <main className="wrapper">
-        <h2 className="header">About sQuizer</h2>
-        <h2 className="header">Implemented features</h2>
+        <Heading>About sQuizer</Heading>
+        <Heading>Implemented features</Heading>
         <ul className="list">
-          <li className="list__item">Login and registration</li>
-          <li className="list__item">JWT + cookie based security</li>
-          <li className="list__item">Basic Accessibility</li>
-          <li className="list__item">Quizzes creating and deleting</li>
-          <li className="list__item">Scoring and user ranking system</li>
-          <li className="list__item">Comments adding</li>
-          <li className="list__item">Avatar updating</li>
+          <ListItem>Login and registration</ListItem>
+          <ListItem>JWT + cookie based security</ListItem>
+          <ListItem>Basic Accessibility</ListItem>
+          <ListItem>Quizzes creating and deleting</ListItem>
+          <ListItem>Scoring and user ranking system</ListItem>
+          <ListItem>Comments adding</ListItem>
+          <ListItem>Avatar updating</ListItem>
         </ul>
 
-        <h2 className="header">Why did I create sQuizer?</h2>
+        <Heading>Why did I create sQuizer?</Heading>
         <p>
           In my previous project
-          <a className="link" href="https://github.com/krzysztof01-sz/csale-react-olx-clone">
+          <a className="link" href="https://burza-idzie.web.app">
             <i>(Csale)</i>
           </a>{' '}
-          I was working with firebase. I turned out to be a really powerful technology but I was a
-          bit disgusted that I didn't know how it works. I mean I knew nothing about authentication,
-          security and working with backend. So in order to learn it I decided to create a small
-          project which later became more complex. Finally, I've learned a lot of thing doing this
+          I was working with firebase. It turned out to be a really powerful technology but I was a bit disgusted that I didn't
+          know how it works. I mean I knew nothing about authentication, security and working with backend. So in order to learn
+          it I decided to create a small project which later became more complex. Finally, I've learned a lot of thing doing this
           project so I achieved my basic goal.
         </p>
 
-        <h2 className="header">Am I planning to add new features?</h2>
+        <Heading>Am I planning to add new features?</Heading>
         <p>
-          Yes, I'm planning to add better authentication because now I still use partially
-          localStorage for handling user authentication on the frontend side (backend has
-          cookie-based auth) or totally change it on cookie + session. Also comments deleting and
-          random quiz generation. But I don't guarantee that I'll be done in 100% 😄.
+          Yes, I'm planning to add better authentication because now I still use partially localStorage for handling user
+          authentication on the frontend side (backend has cookie-based auth) or totally change it on cookie + session. Also
+          comments deleting and random quiz generation. But I don't guarantee that I'll be done in 100% 😄.
         </p>
 
-        <h2 className="header">If you want to check my github repos:</h2>
+        <Heading>If you want to check my github repos:</Heading>
         <ul className="list">
-          <li className="list__item">
+          <ListItem>
             <a className="link" href="https://github.com/krzysztof01-sz/squizer-app-frontend">
               Frontend
             </a>
-          </li>
-          <li className="list__item">
+          </ListItem>
+          <ListItem>
             <a className="link" href="https://github.com/krzysztof01-sz/squizer-app-backend">
-              backend
+              Backend
             </a>
-          </li>
+          </ListItem>
         </ul>
       </main>
     </Layout>
