8385b98bd9ce68e5a4b4b7cbff7cc8f79c57cf15
Krzysztof <krzysztofs012@outlook.com>
2021-05-23 11:09:27 +0200

    Changed AuthContext to UserContext

--- src/global/Components/Layout/index.js
+++ src/global/Components/Layout/index.js
@@ -1,18 +1,18 @@
 import { useContext, useEffect } from 'react';
-import { AuthContext } from '../../../contexts/Auth';
+import { UserContext } from '../../../contexts/User';
 import { navItems } from '../../../utils/constants';
 import Footer from '../Footer';
 import Header from '../Header';
 import MobileNav from '../MobileNav';
 
 const Layout = ({ children }) => {
-  const { isLogged } = useContext(AuthContext);
+  const { user } = useContext(UserContext);
   useEffect(() => window.scrollTo(0, 0), []);
 
   return (
     <>
       <Header />
-      {isLogged ? <MobileNav navItems={navItems} /> : null}
+      {user ? <MobileNav navItems={navItems} /> : null}
       {children}
       <Footer />
     </>
