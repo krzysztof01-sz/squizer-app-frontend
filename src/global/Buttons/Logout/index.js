19f2bf9f28428a53a146917e1b466a2d6d0980ac
Krzysztof <krzysztofs012@outlook.com>
2021-05-23 11:07:51 +0200

    Changed logout handler (changed authentication)

--- src/global/Buttons/Logout/index.js
+++ src/global/Buttons/Logout/index.js
@@ -1,20 +1,18 @@
-import { useContext } from 'react';
 import * as api from '../../../api';
-import { AuthContext } from '../../../contexts/Auth';
 import { LOGOUT_CONFIRMATION } from '../../../utils/feedbackMessages';
 import './styles.scss';
 
 const LogoutButton = () => {
-  const { setIsLogged } = useContext(AuthContext);
   return (
     <button
       className="button logoutButton"
-      onClick={() => {
+      onClick={async () => {
         if (confirm(LOGOUT_CONFIRMATION)) {
-          localStorage.removeItem('isLogged');
-          setIsLogged(false);
-          return api.logoutUser();
-        } else return false;
+          const { success } = await api.logoutUser();
+          if (success) location.assign('/login');
+        } else {
+          return false;
+        }
       }}
     >
       <p>Logout</p>
