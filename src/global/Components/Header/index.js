c54c77262ed2db7739a931b4af5acc45881ebd11
Krzysztof <krzysztofs012@outlook.com>
2021-05-23 11:09:10 +0200

    Changed AuthContext to UserContext

--- src/global/Components/Header/index.js
+++ src/global/Components/Header/index.js
@@ -4,20 +4,21 @@ import Logo from '../../../assets/images/Logo.svg';
 import AccessibleWrapper from '../../AccessibleWrapper';
 import Nav from './Nav';
 import HeaderButton from '../../Buttons/HeaderButton';
-import { AuthContext } from '../../../contexts/Auth';
 import { navItems } from '../../../utils/constants';
+import { UserContext } from '../../../contexts/User';
 import './styles.scss';
 
 const Header = () => {
-  const { isLogged } = useContext(AuthContext);
+  const { user } = useContext(UserContext);
+
   return (
     <header className="header">
       <AccessibleWrapper>
-        <Link to={isLogged ? '/dashboard' : '/'}>
+        <Link to={user ? '/dashboard' : '/'}>
           <img className="header__logo" src={Logo} alt="logo" />
         </Link>
       </AccessibleWrapper>
-      {isLogged ? (
+      {user ? (
         <Nav navItems={navItems} />
       ) : (
         <section className="header__actionButtons">
