1ee99e3e9ae18f6e875eeb030cfd34e5c9860730
Krzysztof <krzysztofs012@outlook.com>
2021-05-24 17:42:21 +0200

    Changed prop names and added docs

--- src/global/AccessibleWrapper/index.js
+++ src/global/AccessibleWrapper/index.js
@@ -1,9 +1,18 @@
-const AccessibleWrapper = ({ children }) => {
+/* 
+This component enables to make elements clickable and focusable.
+The "withTabIndex" props means: Does the WrappedComponent should be focusable
+The "clickable" props means: Does the WrappedComponent should be clickable
+Usage: 
+  - to make links clickable when they are currently focused
+  - to make button-like elements clickable and focusable
+*/
+
+const AccessibleWrapper = ({ children, withTabIndex = true, clickable = true }) => {
   return (
     <div
       className="accessibleWrapper"
-      tabIndex="0"
-      onKeyDown={(e) => (e.key === 'Enter' ? e.target.firstElementChild?.click() : false)}
+      tabIndex={withTabIndex ? 0 : -1}
+      onKeyDown={(e) => (e.key === 'Enter' && clickable ? e.target.firstElementChild?.click() : false)}
       role="contentinfo"
     >
       {children}
