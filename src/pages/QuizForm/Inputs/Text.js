5a1807c5a7ece2e2240fc25288c619a714700669
Krzysztof <krzysztofs012@outlook.com>
2021-05-24 19:36:02 +0200

    Added borderColor prop to improve UX

--- src/pages/QuizForm/Inputs/Text.js
+++ src/pages/QuizForm/Inputs/Text.js
@@ -1,6 +1,6 @@
 import '../../../global/Components/Input/styles.scss';
 
-const Input = ({ name, labelName, value, callback }) => {
+const Input = ({ name, labelName, value, callback, borderColor = '#dbdbdb' }) => {
   return (
     <label aria-label={labelName} className="label form__label quizForm__label">
       {labelName} <br />
@@ -8,6 +8,7 @@ const Input = ({ name, labelName, value, callback }) => {
         className="input form__input quizForm__input"
         type="text"
         required
+        style={{ borderBottom: `2px solid ${borderColor}` }}
         name={name}
         value={value}
         onChange={callback}
