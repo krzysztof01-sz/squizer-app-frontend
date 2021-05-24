4161cd70b65025c8161670ee1fafe60c5e4339e7
Krzysztof <krzysztofs012@outlook.com>
2021-05-24 19:35:15 +0200

    Separated business logic into useQuizForm hook

--- src/pages/QuizForm/Form/index.js
+++ src/pages/QuizForm/Form/index.js
@@ -10,138 +10,35 @@ import Input from '../Inputs/Text';
 import SelectInput from '../Inputs/Select';
 import TextAreaInput from '../Inputs/Textarea';
 
-import * as fb from '../../../utils/feedbackMessages';
 import ErrorMessage from '../../../global/Components/Messages/ErrorMessage';
 import ActionResultMessage from '../../../global/Components/Messages/ActionResultMessage';
 
-import ErrorPage from '../../ErrorPage';
 import { quizCategories, responseTypes } from '../../../utils/constants';
 import Layout from '../../../global/Components/Layout';
+import { useQuizForm } from '../useQuizForm';
+import { colors } from '../../../utils/constants';
+
+const focusOnFirstInput = () => document.querySelector('textarea[name="content"]').focus();
 
 const Form = () => {
-  const [generalQuizData, setGeneralQuizData] = useState({
-    title: '',
-    category: '',
-    description: '',
-  });
-  const initialQuestionData = {
-    content: '',
-    correctAnswer: '',
-    optionalAnswer1: '',
-    optionalAnswer2: '',
-    optionalAnswer3: '',
-  };
   const history = useHistory();
-  const initialQuestionID = 0;
-  const [questions, setQuestions] = useState([{ ...initialQuestionData }]);
-  const [questionID, setQuestionID] = useState(initialQuestionID);
   const [isSubmitting, setIsSubmitting] = useState(false);
-  const [authError, setAuthError] = useState(null);
-  const [messages, setMessages] = useState([]);
-  const [validationErrors, setValidationErrors] = useState([]);
+  const [serverErrors, setServerErrors] = useState([]);
+  const [clientErrors, setClientErrors] = useState([]);
 
-  const getCurrentQuestionsContents = () => {
-    const currentQuestionsContents = [];
-    questions.forEach(({ content }) => currentQuestionsContents.push(content));
-    return [...currentQuestionsContents];
-  };
-
-  const isQuestionUnique = (questionContent) => {
-    const currentQuestionsContents = getCurrentQuestionsContents();
-    const incluesAtLeast2Elements = currentQuestionsContents.length >= 2;
-    if (incluesAtLeast2Elements) {
-      currentQuestionsContents.length = questionID;
-      return currentQuestionsContents.includes(questionContent) ? false : true;
-    } else return true;
-  };
-
-  const areAnswersUnique = (answers) => {
-    const properUniqueAnswersQuantity = answers.length;
-    const uniqueAnswersQuantity = Array.from(new Set(answers)).length;
-    return uniqueAnswersQuantity === properUniqueAnswersQuantity;
-  };
-
-  const trimObjectFields = (obj) => {
-    const objWithTrimmedValues = {};
-    for (const [key, value] of Object.entries(obj)) {
-      objWithTrimmedValues[key] = value.trim();
-    }
-    return objWithTrimmedValues;
-  };
-
-  const areAllFieldsFilled = (question) => {
-    const questionWithTrimmedValues = trimObjectFields({ ...question });
-    const trimmedValues = Object.values(questionWithTrimmedValues);
-    const allFieldsFilled = !trimmedValues.includes('');
-    return allFieldsFilled;
-  };
-
-  const isQuestionValid = (question) => {
-    const questionCopy = { ...question };
-    const questionUnique = isQuestionUnique(questionCopy.content);
-    const allFieldsFilled = areAllFieldsFilled(questionCopy);
-
-    delete questionCopy.content;
-    const questionAnswers = Object.values(questionCopy);
-
-    const answersUnique = areAnswersUnique(questionAnswers);
-    const messages = [];
-
-    if (!questionUnique) messages.push(fb.QUESTION_REPEATED);
-    if (!allFieldsFilled) messages.push(fb.NOT_ALL_FILLED);
-    if (!answersUnique) messages.push(fb.NOT_UNIQUE_ANSWERS);
-
-    return messages.length === 0 ? true : messages;
-  };
-
-  const getRandomNumber = () => Math.random().toString();
-
-  const setQuestionData = (value, name) => {
-    setQuestions(() => {
-      questions[questionID][name] = value;
-      return [...questions];
-    });
-  };
-
-  const prepareQuestionStructure = () => {
-    const generalQuizDataFilled = areAllFieldsFilled(generalQuizData);
-
-    if (generalQuizDataFilled) {
-      const generalData = trimObjectFields(generalQuizData);
-      const questionsWithTrimmedFields = questions.map((question) => trimObjectFields(question));
-
-      const structuredQuestions = [];
-      questionsWithTrimmedFields.forEach(
-        ({ content, correctAnswer, optionalAnswer1, optionalAnswer2, optionalAnswer3 }) => {
-          const correctAnswerID = getRandomNumber();
-          structuredQuestions.push({
-            content: content,
-            answers: [
-              { answerId: correctAnswerID, content: correctAnswer },
-              { answerId: getRandomNumber(), content: optionalAnswer1 },
-              { answerId: getRandomNumber(), content: optionalAnswer2 },
-              { answerId: getRandomNumber(), content: optionalAnswer3 },
-            ],
-            correctAnswer: correctAnswerID,
-          });
-        },
-      );
-      structuredQuestions.pop();
-      const quiz = { ...generalData, questions: [...structuredQuestions] };
-      return quiz;
-    }
-  };
-
-  const focusOnFirstInput = () => document.querySelector('input[name="content"]').focus();
-
-  const userCanAddAQuiz = () => {
-    const thereAreAtLeast2QuestionsSet = questionID >= 2;
-    const userIsFillingTheNewestQuestion = !questions[questionID + 1];
-
-    return thereAreAtLeast2QuestionsSet && userIsFillingTheNewestQuestion;
-  };
-
-  if (authError) return <ErrorPage msg={authError} />;
+  const {
+    generalQuizData,
+    setGeneralQuizData,
+    questions,
+    setQuestions,
+    questionID,
+    setQuestionID,
+    initialQuestionData,
+    setQuestionData,
+    isQuestionValid,
+    canUserAddQuiz,
+    prepareQuestionStructure,
+  } = useQuizForm();
 
   const currentQuestion = questions[questionID];
 
@@ -155,9 +52,7 @@ const Form = () => {
               name="title"
               labelName="title"
               value={generalQuizData?.title}
-              callback={({ target }) =>
-                setGeneralQuizData({ ...generalQuizData, [target.name]: target.value })
-              }
+              callback={({ target }) => setGeneralQuizData({ ...generalQuizData, [target.name]: target.value })}
             />
 
             <SelectInput
@@ -166,9 +61,7 @@ const Form = () => {
               name="category"
               options={quizCategories}
               value={generalQuizData?.category}
-              callback={({ target }) =>
-                setGeneralQuizData({ ...generalQuizData, [target.name]: target.value })
-              }
+              callback={({ target }) => setGeneralQuizData({ ...generalQuizData, [target.name]: target.value })}
             />
 
             <TextAreaInput
@@ -177,42 +70,46 @@ const Form = () => {
               rows="5"
               cols="40"
               value={generalQuizData?.description}
-              callback={({ target }) =>
-                setGeneralQuizData({ ...generalQuizData, [target.name]: target.value })
-              }
+              callback={({ target }) => setGeneralQuizData({ ...generalQuizData, [target.name]: target.value })}
             />
           </section>
 
           <section className="questionsPart">
-            <Input
+            <TextAreaInput
               name="content"
               labelName="question"
+              rows="5"
+              cols="40"
               value={currentQuestion?.content}
               callback={({ target: { value, name } }) => setQuestionData(value, name)}
             />
             <Input
+              borderColor={colors.valid}
               name="correctAnswer"
-              labelName="correct answer"
+              labelName="put the correct answer"
               value={currentQuestion?.correctAnswer}
               callback={({ target: { value, name } }) => setQuestionData(value, name)}
             />
             <Input
+              borderColor={colors.invalid}
               name="optionalAnswer1"
-              labelName="1st optional answer"
+              labelName="1st incorrect answer"
               value={currentQuestion?.optionalAnswer1}
               callback={({ target: { value, name } }) => setQuestionData(value, name)}
             />
 
             <Input
+              borderColor={colors.invalid}
               name="optionalAnswer2"
-              labelName="2nd optional answer"
+              labelName="2nd incorrect answer"
               value={currentQuestion?.optionalAnswer2}
               callback={({ target: { value, name } }) => setQuestionData(value, name)}
             />
 
             <Input
+              borderColor={colors.invalid}
               name="optionalAnswer3"
-              labelName="3rd optional answer"
+              labelName="3rd incorrect answer"
               value={currentQuestion?.optionalAnswer3}
               callback={({ target: { value, name } }) => setQuestionData(value, name)}
             />
@@ -221,11 +118,11 @@ const Form = () => {
               question no. {questionID + 1}
             </p>
 
-            {validationErrors.map((error, index) => {
+            {clientErrors.map((error, index) => {
               return <ErrorMessage key={index} message={error} />;
             })}
 
-            {messages.map(({ msg, type }, index) => {
+            {serverErrors.map(({ msg, type }, index) => {
               return <ActionResultMessage key={index} msg={msg} type={type} />;
             })}
 
@@ -245,7 +142,7 @@ const Form = () => {
                 <AddQuestionButton
                   callback={(e) => {
                     e.preventDefault();
-                    setValidationErrors([]);
+                    setClientErrors([]);
                     if (questionID < questions.length) {
                       const validationResult = isQuestionValid(currentQuestion);
 
@@ -257,7 +154,7 @@ const Form = () => {
                         }
                       } else {
                         focusOnFirstInput();
-                        return setValidationErrors(validationResult);
+                        return setClientErrors(validationResult);
                       }
                     }
                     setQuestionID(questionID + 1);
@@ -267,25 +164,18 @@ const Form = () => {
               </div>
             </section>
             <aside className="buttonWrapper">
-              {userCanAddAQuiz() ? (
+              {canUserAddQuiz() ? (
                 <AddQuizButton
                   isActive={isSubmitting}
                   callback={async () => {
                     setIsSubmitting(true);
                     const quiz = prepareQuestionStructure();
-                    const { type, msg } = await api.addQuiz(quiz);
+                    const response = await api.addQuiz(quiz);
 
-                    // when token is invalid, middleware returns plain text, so here I preserve it
-                    const isAuthError = type === responseTypes.error && typeof msg === 'string';
-
-                    if (isAuthError) {
-                      setAuthError(msg);
-                    } else {
-                      setMessages(msg);
-                    }
-
-                    if (type === responseTypes.success) return history.push('/dashboard');
                     setIsSubmitting(false);
+                    setServerErrors(response.msg);
+
+                    if (response.type === responseTypes.success) return history.push('/dashboard');
                   }}
                 />
               ) : null}
