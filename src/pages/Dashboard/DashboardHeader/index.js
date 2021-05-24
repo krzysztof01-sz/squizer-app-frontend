3240a3787e4dfde599921d7ae01ea2c44cb35037
Krzysztof <krzysztofs012@outlook.com>
2021-05-24 19:26:48 +0200

    Removed unnecessary code

--- src/pages/Dashboard/DashboardHeader/index.js
+++ src/pages/Dashboard/DashboardHeader/index.js
@@ -1,17 +1,14 @@
 import { Link } from 'react-router-dom';
 import { PlusIcon } from '../../../global/Icons';
-import AccessibleWrapper from '../../../global/AccessibleWrapper';
 import './styles.scss';
 
 const DashboardHeader = () => {
   return (
     <header className="dashboardHeader">
       <h1 className="dashboardHeader__title">Quizzes</h1>
-      <AccessibleWrapper>
-        <Link to="/quizform">
-          Add a quiz <PlusIcon />
-        </Link>
-      </AccessibleWrapper>
+      <Link to="/quizform">
+        Add a quiz <PlusIcon />
+      </Link>
     </header>
   );
 };
