479700853e9c29e4a2f9751ce8e0a5e12e584442
Krzysztof <krzysztofs012@outlook.com>
2021-05-24 19:37:47 +0200

    Removed unnecessary code

--- src/pages/QuizGame/QuestionContent/index.js
+++ src/pages/QuizGame/QuestionContent/index.js
@@ -2,7 +2,7 @@ import './styles.scss';
 
 const QuestionContent = ({ questionID, content }) => {
   return (
-    <p tabIndex="0" data-questionid={questionID + 1} className="question__content">
+    <p data-questionid={questionID + 1} className="question__content">
       {content}
     </p>
   );
