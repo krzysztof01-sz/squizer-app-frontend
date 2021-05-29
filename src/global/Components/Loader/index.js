b9387065b45ceabe4cee02d43401b9003e15bee5
Krzysztof <krzysztofs012@outlook.com>
2021-05-29 10:34:40 +0200

    Added new loader, user-friendly image (when user is waiting too much time) and added new styling

--- src/global/Components/Loader/index.js
+++ src/global/Components/Loader/index.js
@@ -1,15 +1,28 @@
-import LoaderImg from '../../../assets/images/Loader.svg';
+import { useEffect, useState } from 'react';
+import Spinner from 'react-loading';
+import { colors } from '../../../utils/constants';
+import WaitImg from '../../../assets/images/Wait.svg';
 import './styles.scss';
 
 const Loader = ({ width = 100, height = 100 }) => {
+  const [shouldRender, setShouldRender] = useState(false);
+
+  useEffect(() => {
+    const loadingTimer = setTimeout(() => setShouldRender(true), 3000);
+    return () => clearTimeout(loadingTimer);
+  }, []);
+
   return (
     <div className="loader">
-      <img
-        style={{ width: `${width}px`, height: `${height}px` }}
-        src={LoaderImg}
-        alt="loader"
-        className="loader__img"
-      />
+      {shouldRender ? (
+        <>
+          <h2 className="loader__message">Wait for a moment, loading is in progress...</h2>
+          <h2 className="loader__tip">Check your internet hook-up</h2>
+          <img alt="Loading image" className="loader__timeImage" src={WaitImg} />
+        </>
+      ) : (
+        <Spinner className="loader__image" type="bubbles" width={width} height={height} color={colors.primary} />
+      )}
     </div>
   );
 };
