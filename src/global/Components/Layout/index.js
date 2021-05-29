d298cf6a5ba6c9f8a343c05b5e87b503c423e3f2
Krzysztof <krzysztofs012@outlook.com>
2021-05-29 10:37:15 +0200

    Fixed bug with redirecting after losing the global user context

--- src/global/Components/Layout/index.js
+++ src/global/Components/Layout/index.js
@@ -1,13 +1,34 @@
 import { useContext, useEffect } from 'react';
+import { useHistory, useLocation } from 'react-router';
 import { UserContext } from '../../../contexts/User';
-import { navItems } from '../../../utils/constants';
+import { navItems, unauthenticatedRoutes } from '../../../utils/constants';
 import Footer from '../Footer';
 import Header from '../Header';
 import MobileNav from '../MobileNav';
+import { useAuth } from '../../../hooks/useAuth';
 
 const Layout = ({ children }) => {
+  const history = useHistory();
+  const location = useLocation();
   const { user } = useContext(UserContext);
+  const { refetchUser } = useAuth();
+
   useEffect(() => window.scrollTo(0, 0), []);
+
+  useEffect(() => {
+    const currentRoute = history.location.pathname;
+
+    if (!unauthenticatedRoutes.includes(currentRoute)) {
+      localStorage.setItem('current-route', currentRoute);
+    }
+  }, [location]);
+
+  useEffect(() => {
+    if (!user) {
+      (() => refetchUser())();
+      return () => false;
+    }
+  }, [user]);
 
   return (
     <>
