e65533b79d081b0de8b4e0b7bea94fdb892ee1f0
Krzysztof <krzysztofs012@outlook.com>
2021-06-08 15:51:30 +0200

    Added a 'visited' badge to the card, corrected styling and RWD

--- src/pages/Dashboard/QuizCard/index.js
+++ src/pages/Dashboard/QuizCard/index.js
@@ -1,5 +1,5 @@
 import QuizButton from './QuizButton';
-import { categoryToColor as map, maxDescrptionLengthOnCard } from '../../../utils/constants';
+import { categoryToColor as map, maxDescriptionLengthOnCard, maxTitleLengthOnCard } from '../../../utils/constants';
 import { formatDate, shortenText } from '../../../utils/functions';
 import { useCategoryImage } from '../../../hooks/useCategoryImage';
 import { memo } from 'react';
@@ -7,20 +7,26 @@ import { useFetching } from '../../../hooks/useFetching';
 import { getUser } from '../../../api';
 import { Skeleton } from '@material-ui/lab';
 import './styles.scss';
+import { CheckIcon } from '../../../global/Icons';
 
-const QuizCard = ({ quiz: { _id, title, description, creationDate, createdBy, category } }) => {
+const QuizCard = ({ isVisited, quiz: { _id, title, description, creationDate, createdBy, category } }) => {
   const { categoryImage } = useCategoryImage(category);
   const { data: user } = useFetching(getUser, createdBy);
 
   return user && categoryImage ? (
     <article role="listitem" className="quizCard" style={{ borderBottom: `10px solid ${map.get(category)}` }}>
+      {isVisited && (
+        <div className="quizCard__badge">
+          Visited <CheckIcon />
+        </div>
+      )}
       <div className="quizCard__categoryImageWrapper">
         <img className="quizCard__categoryImage" src={categoryImage} alt={category} />
         <img className="quizCard__avatar" src={user.avatar} alt={`${user.nickname} avatar`} />
       </div>
       <div role="contentinfo" className="quizCard__info">
-        <p className="quizCard__title">{title}</p>
-        <p className="quizCard__description">{shortenText(description, maxDescrptionLengthOnCard)}</p>
+        <p className="quizCard__title">{shortenText(title, maxTitleLengthOnCard)}</p>
+        <p className="quizCard__description">{shortenText(description, maxDescriptionLengthOnCard)}</p>
         <div className="quizCard__buttonAndDateWrapper">
           <QuizButton borderColor={map.get(category)} quizId={_id} />
           <p className="quizCard__creationDate">{formatDate(creationDate)}</p>
