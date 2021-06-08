64d4537fc53493fff1723f458f77aed4240129cd
Krzysztof <krzysztofs012@outlook.com>
2021-06-08 15:49:01 +0200

    Added information about the average of ratings and styled it

--- src/pages/AboutQuiz/PresentationCard/index.js
+++ src/pages/AboutQuiz/PresentationCard/index.js
@@ -1,11 +1,25 @@
 import { memo } from 'react';
 import { useParams } from 'react-router-dom';
-import { formatDate } from '../../../utils/functions';
+import AccessibleWrapper from '../../../global/AccessibleWrapper';
+import { formatDate, getStarsRenderingOrder } from '../../../utils/functions';
+import Star from '../../QuizGame/RatingCard/Star';
 import PlayButton from './PlayButton';
 import './styles.scss';
 
+const getRatingValues = (ratings) => ratings.map(({ rating }) => rating);
+const getAverage = (values) => values.reduce((prev, acc) => prev + acc, 0);
+
+const getAverageRating = (ratings) => {
+  if (ratings.length === 0) return 0;
+
+  const ratingValues = getRatingValues(ratings);
+  const average = getAverage(ratingValues);
+  return (average / ratings.length).toFixed(2);
+};
+
 const PresentationCard = ({ card: { quiz, user, categoryImage } }) => {
   const { quizId } = useParams();
+  const quizRating = getAverageRating(quiz.ratings);
 
   const backgroundStyles = {
     backgroundImage: `url(${categoryImage})`,
@@ -23,6 +37,14 @@ const PresentationCard = ({ card: { quiz, user, categoryImage } }) => {
           </div>
           <h1 className="quizCard__title">{quiz.title}</h1>
           <p className="quizCard__description">{quiz.description}</p>
+          <p>
+            Average: {quizRating}, rating(s): {quiz.ratings.length}
+          </p>
+          <section className="quizCard__starsWrapper">
+            {getStarsRenderingOrder(quizRating).map(({ isLightStar: isLight, id }) => (
+              <Star isLight={isLight} key={id} callback={() => false} />
+            ))}
+          </section>
           <div className="buttonAndInfoWrapper">
             <PlayButton link={`/quiz/${quizId}/play`} />
             <div className="infoWrapper">
