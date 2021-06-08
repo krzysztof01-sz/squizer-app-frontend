93fea52eeb3c4dbf020a63b6f6fd59ae92b37045
Krzysztof <krzysztofs012@outlook.com>
2021-06-08 15:52:06 +0200

    Added a 'visited' badge to the quizCard component

--- src/pages/Dashboard/QuizzesList/index.js
+++ src/pages/Dashboard/QuizzesList/index.js
@@ -1,18 +1,20 @@
 import { useContext } from 'react';
 import { FilteringContext } from '../../../contexts/Filtering';
+import { UserContext } from '../../../contexts/User';
 import LackOfQuizzesView from '../LackOfQuizzesView';
 import QuizCard from '../QuizCard';
 import './styles.scss';
 
 const QuizzesList = ({ quizzes }) => {
   const { invisibleQuizzesQuantity } = useContext(FilteringContext);
+  const { user: me } = useContext(UserContext);
 
   return (
     <>
-      {invisibleQuizzesQuantity === 0 ? <LackOfQuizzesView /> : null}
+      {invisibleQuizzesQuantity === 0 && <LackOfQuizzesView />}
       <section role="list" className="quizzesList">
         {quizzes.map((quiz) => {
-          return <QuizCard key={quiz._id} quiz={quiz} />;
+          return <QuizCard isVisited={me.visitedQuizzes.includes(quiz._id)} key={quiz._id} quiz={quiz} />;
         })}
       </section>
     </>
