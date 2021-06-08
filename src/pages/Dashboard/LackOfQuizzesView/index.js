392c9b3e9165e85a9875a708970db48110dec0cc
Krzysztof <krzysztofs012@outlook.com>
2021-06-08 15:50:01 +0200

    Added and styled an errorImg component

--- src/pages/Dashboard/LackOfQuizzesView/index.js
+++ src/pages/Dashboard/LackOfQuizzesView/index.js
@@ -1,9 +1,11 @@
+import ErrorImg from '../../../assets/images/Resource-not-found.svg';
 import './styles.scss';
 
 const LackOfQuizzesView = () => {
   return (
     <section className="lackOfQuizzes">
       <p className="lackOfQuizzes__message">There is no such a quiz 🙁</p>
+      <img alt="Lack of quizzes" className="lackOfQuizzes__image" src={ErrorImg} />
     </section>
   );
 };
