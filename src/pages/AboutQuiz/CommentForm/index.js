c486a880314dc8c2b69f937af7813607390139d2
Krzysztof <krzysztofs012@outlook.com>
2021-06-08 15:48:11 +0200

    Changed import path

--- src/pages/AboutQuiz/CommentForm/index.js
+++ src/pages/AboutQuiz/CommentForm/index.js
@@ -3,7 +3,7 @@ import { useParams } from 'react-router-dom';
 import ActionResultMessage from '../../../global/Components/Messages/ActionResultMessage';
 import AddCommentButton from './AddCommentButton';
 import TextArea from './TextArea';
-import CharactersCounter from './CharactersCounter';
+import CharactersCounter from '../../../global/Components/CharactersCounter';
 import { useComment } from '../../../hooks/useComment';
 
 const CommentForm = () => {
